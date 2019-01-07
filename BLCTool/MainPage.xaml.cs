d5ac48b7a8a732e033c2f498c262fef3add8ea42
David Nankivell <David.Nanki@gmail.com>
2019-01-08 00:18:26 +0800

    Initial Commit

---
+++ BLCTool/MainPage.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+using Microsoft.Win32;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace BLCTool
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            
+
+            LoadPath();
+        }
+
+        private void LoadPath()
+        {
+            RegistryKey regKey = Registry.LocalMachine.OpenSubKey(@"Software\Microsoft\Windows\CurrentVersion\Uninstall");
+            string location = FindByDisplayName(regKey, "Borderlands");
+            PathTarget.Text = location;
+        }
+
+        private string FindByDisplayName(RegistryKey parentKey, string name)
+        {
+            string[] nameList = parentKey.GetSubKeyNames();
+            for (int i = 0; i < nameList.Length; i++)
+            {
+                RegistryKey regKey = parentKey.OpenSubKey(nameList[i]);
+                try
+                {
+                    if (regKey.GetValue("DisplayName").ToString() == name)
+                    {
+                        return regKey.GetValue("InstallLocation").ToString();
+                    }
+                }
+                catch
+                {
+
+                }
+            }
+            return "";
+        }
+    }
+}
